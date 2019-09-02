69ebf8c7b84518370b47675b2179bac16106063c
Florin <ciolea.florin.99@gmail.com>
2019-09-02 21:02:27 +0300

    added constraint to avoid having auto-vehicles without engine (studied constructors)

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,25 +2,27 @@ package org.fasttrackit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         //one-line comment
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1600;
 
-        Car car=new Car();
-        car.name="Dacia";
-        car.fuelLevel=60;
-        car.mileage=8.5;
-        car.doorCount=5;
-        car.maxSpeed=180;
-        car.fuelType="Gasoline";
-        Engine engine=new Engine();
-        engine.manufacturer="Renault";
-        engine.capacity=1600;
-        car.engine=engine;
+
+        Engine engine2=new Engine();
+        engine2.manufacturer="VW";
+        engine2.capacity=1800;
+        Car car = new Car(engine2);
+        car.name = "Dacia";
+        car.fuelLevel = 60;
+        car.mileage = 8.5;
+        car.doorCount = 5;
+        car.maxSpeed = 180;
+        car.fuelType = "Gasoline";
+
+
 
         System.out.println(car.name);
         System.out.println(car.travelDistance);
@@ -28,22 +30,19 @@ public class App
 
         car.accelerate(60, 1);
 
-        Car car2=new Car();
-        car2.name="Golf";
-        car2.mileage=6.5;
-        car2.doorCount=3;
-        car2.maxSpeed=160;
-        car2.fuelLevel=55;
+        Car car2 = new Car(engine);
+        car2.name = "Golf";
+        car2.mileage = 6.5;
+        car2.doorCount = 3;
+        car2.maxSpeed = 160;
+        car2.fuelLevel = 55;
 
 //        car.name=null;
         //concatenare
         System.out.println("Car2 name: " + car2.name);
         System.out.println("Car1 name: " + car.name);
 
-        car=car2;
-
-
-
+        car = car2;
 
 
     }
